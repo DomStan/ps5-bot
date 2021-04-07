1f83986aa24ee794c0697c5be9d375bde3e19166
DomStan <dominykas.stankevicius@warwick.ac.uk>
2021-04-08 00:02:46 +0300

    Added firefox path

--- ps5.py
+++ ps5.py
@@ -17,13 +17,13 @@ from selenium.webdriver.firefox.options import Options
 from xvfbwrapper import Xvfb
 
 # web driver setup
-xvfb = Xvfb(display=99)
+xvfb = Xvfb(width=1280, height=720)
 xvfb.start()
 
 options = Options()
 # option for headless mode
 options.add_argument("-headless")
-DRIVER = webdriver.Firefox(firefox_binary='./firefox/firefox', executable_path='./geckodriver', options=options)
+DRIVER = webdriver.Firefox(executable_path='./geckodriver', options=options)
 #
 
 # logging setup
