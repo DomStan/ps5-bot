3cf9a5708426b1070dfe6c265e6866c689df1e3e
DomStan <dominykas.stankevicius@warwick.ac.uk>
2021-04-08 11:19:43 +0300

    Added firefox profile to speed up page loading

--- ps5.py
+++ ps5.py
@@ -20,49 +20,49 @@ from xvfbwrapper import Xvfb
 # xvfb = Xvfb(width=1280, height=720)
 # xvfb.start()
 
-# options = Options()
-# options.add_argument("--headless")
-#
-# profile = webdriver.FirefoxProfile()
-# profile.set_preference("network.http.pipelining", True)
-# profile.set_preference("network.http.proxy.pipelining", True)
-# profile.set_preference("network.http.pipelining.maxrequests", 8)
-# profile.set_preference("content.notify.interval", 500000)
-# profile.set_preference("content.notify.ontimer", True)
-# profile.set_preference("content.switch.threshold", 250000)
-# profile.set_preference("browser.cache.memory.capacity", 65536) # Increase the cache capacity.
-# profile.set_preference("browser.startup.homepage", "about:blank")
-# profile.set_preference("reader.parse-on-load.enabled", False) # Disable reader, we won't need that.
-# profile.set_preference("browser.pocket.enabled", False) # Duck pocket too!
-# profile.set_preference("loop.enabled", False)
-# profile.set_preference("browser.chrome.toolbar_style", 1) # Text on Toolbar instead of icons
-# profile.set_preference("browser.display.show_image_placeholders", False) # Don't show thumbnails on not loaded images.
-# profile.set_preference("browser.display.use_document_colors", False) # Don't show document colors.
-# profile.set_preference("browser.display.use_document_fonts", 0) # Don't load document fonts.
-# profile.set_preference("browser.display.use_system_colors", True) # Use system colors.
-# profile.set_preference("browser.formfill.enable", False) # Autofill on forms disabled.
-# profile.set_preference("browser.helperApps.deleteTempFileOnExit", True) # Delete temprorary files.
-# profile.set_preference("browser.shell.checkDefaultBrowser", False)
-# profile.set_preference("browser.startup.homepage", "about:blank")
-# profile.set_preference("browser.startup.page", 0) # blank
-# profile.set_preference("browser.tabs.forceHide", True) # Disable tabs, We won't need that.
-# profile.set_preference("browser.urlbar.autoFill", False) # Disable autofill on URL bar.
-# profile.set_preference("browser.urlbar.autocomplete.enabled", False) # Disable autocomplete on URL bar.
-# profile.set_preference("browser.urlbar.showPopup", False) # Disable list of URLs when typing on URL bar.
-# profile.set_preference("browser.urlbar.showSearch", False) # Disable search bar.
-# profile.set_preference("extensions.checkCompatibility", False) # Addon update disabled
-# profile.set_preference("extensions.checkUpdateSecurity", False)
-# profile.set_preference("extensions.update.autoUpdateEnabled", False)
-# profile.set_preference("extensions.update.enabled", False)
-# profile.set_preference("general.startup.browser", False)
-# profile.set_preference("plugin.default_plugin_disabled", False)
-# profile.set_preference("permissions.default.image", 2) # Image load disabled again
-#
-# DRIVER = webdriver.Firefox(firefox_profile=profile, firefox_binary='/usr/bin/firefox', executable_path='./geckodriver', options=options)
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.binary_location='/usr/bin/google-chrome'
-DRIVER = webdriver.Chrome(options=chrome_options, executable_path='./chromedriver')
+options = Options()
+options.add_argument("--headless")
+
+profile = webdriver.FirefoxProfile()
+profile.set_preference("network.http.pipelining", True)
+profile.set_preference("network.http.proxy.pipelining", True)
+profile.set_preference("network.http.pipelining.maxrequests", 8)
+profile.set_preference("content.notify.interval", 500000)
+profile.set_preference("content.notify.ontimer", True)
+profile.set_preference("content.switch.threshold", 250000)
+profile.set_preference("browser.cache.memory.capacity", 65536) # Increase the cache capacity.
+profile.set_preference("browser.startup.homepage", "about:blank")
+profile.set_preference("reader.parse-on-load.enabled", False) # Disable reader, we won't need that.
+profile.set_preference("browser.pocket.enabled", False) # Duck pocket too!
+profile.set_preference("loop.enabled", False)
+profile.set_preference("browser.chrome.toolbar_style", 1) # Text on Toolbar instead of icons
+profile.set_preference("browser.display.show_image_placeholders", False) # Don't show thumbnails on not loaded images.
+profile.set_preference("browser.display.use_document_colors", False) # Don't show document colors.
+profile.set_preference("browser.display.use_document_fonts", 0) # Don't load document fonts.
+profile.set_preference("browser.display.use_system_colors", True) # Use system colors.
+profile.set_preference("browser.formfill.enable", False) # Autofill on forms disabled.
+profile.set_preference("browser.helperApps.deleteTempFileOnExit", True) # Delete temprorary files.
+profile.set_preference("browser.shell.checkDefaultBrowser", False)
+profile.set_preference("browser.startup.homepage", "about:blank")
+profile.set_preference("browser.startup.page", 0) # blank
+profile.set_preference("browser.tabs.forceHide", True) # Disable tabs, We won't need that.
+profile.set_preference("browser.urlbar.autoFill", False) # Disable autofill on URL bar.
+profile.set_preference("browser.urlbar.autocomplete.enabled", False) # Disable autocomplete on URL bar.
+profile.set_preference("browser.urlbar.showPopup", False) # Disable list of URLs when typing on URL bar.
+profile.set_preference("browser.urlbar.showSearch", False) # Disable search bar.
+profile.set_preference("extensions.checkCompatibility", False) # Addon update disabled
+profile.set_preference("extensions.checkUpdateSecurity", False)
+profile.set_preference("extensions.update.autoUpdateEnabled", False)
+profile.set_preference("extensions.update.enabled", False)
+profile.set_preference("general.startup.browser", False)
+profile.set_preference("plugin.default_plugin_disabled", False)
+profile.set_preference("permissions.default.image", 2) # Image load disabled again
+
+DRIVER = webdriver.Firefox(firefox_profile=profile, firefox_binary='/usr/bin/firefox', executable_path='./geckodriver', options=options)
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# chrome_options.binary_location='/usr/bin/google-chrome'
+# DRIVER = webdriver.Chrome(options=chrome_options, executable_path='./chromedriver')
 # DRIVER.implicitly_wait(4)
 DRIVER.set_page_load_timeout(5)
 # DRIVER.set_script_timeout(4)
