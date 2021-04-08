081e10303028b9fac45238736396c90a9820ac4f
DomStan <dominykas.stankevicius@warwick.ac.uk>
2021-04-08 12:10:44 +0300

    Added more page load timeouts

--- ps5.py
+++ ps5.py
@@ -57,13 +57,15 @@ profile.set_preference("extensions.update.enabled", False)
 profile.set_preference("general.startup.browser", False)
 profile.set_preference("plugin.default_plugin_disabled", False)
 profile.set_preference("permissions.default.image", 2) # Image load disabled again
+profile.set_preference("http.response.timeout", 3)
+profile.set_preference("dom.max_script_run_time", 3)
 
 DRIVER = webdriver.Firefox(firefox_profile=profile, firefox_binary='/usr/bin/firefox', executable_path='./geckodriver', options=options)
 # chrome_options = Options()
 # chrome_options.add_argument("--headless")
 # chrome_options.binary_location='/usr/bin/google-chrome'
 # DRIVER = webdriver.Chrome(options=chrome_options, executable_path='./chromedriver')
-# DRIVER.implicitly_wait(4)
+DRIVER.implicitly_wait(3)
 DRIVER.set_page_load_timeout(5)
 # DRIVER.set_script_timeout(4)
 #
