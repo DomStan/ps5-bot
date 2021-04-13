76ad469740182d1d77323f646f12dce10f6a75e0
DomStan <dominykas.stankevicius@warwick.ac.uk>
2021-04-13 12:34:43 +0300

    Adjusted page load strategy

--- ps5.py
+++ ps5.py
@@ -71,7 +71,7 @@ PROFILE.set_preference("webgl.disabled", True)
 
 global DRIVER
 DRIVER = webdriver.Firefox(firefox_profile=PROFILE, firefox_binary='/usr/bin/firefox', executable_path='./geckodriver', options=OPTIONS)
-DRIVER.set_page_load_timeout(3)
+DRIVER.set_page_load_timeout(5)
 
 # Logging setup
 logging.basicConfig(filename='logs/' + str(date.today()), format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
@@ -522,7 +522,6 @@ while True:
             DRIVER.get(page.url)
         except TimeoutException:
             logging.warning("Selenium timeout for page: " + page.ID)
-            # continue
         except InvalidSessionIdException:
             logging.error("InvalidSessionIdException. Restarting program.")
             DRIVER.quit()
