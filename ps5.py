7103a2d796cf1f09315e9d94852658d1e02eba36
DomStan <dominykas.stankevicius@warwick.ac.uk>
2021-04-15 12:54:51 +0300

    Changed page load strategy

--- ps5.py
+++ ps5.py
@@ -414,7 +414,7 @@ def extract_text(element):
 def stock_price_from_xpath(driver, page):
     # Wait until the stock element has loaded, then extract it
     try:
-        el = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element_by_xpath(page.stock_xpath))
+        el = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element_by_xpath(page.stock_xpath))
     except Exception:
         pass
     try:
