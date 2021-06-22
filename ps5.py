090c902bf307f6a7a380995d71e47a554ef4951a
DomStan <dominykas.stankevicius@warwick.ac.uk>
2021-06-22 13:59:38 +0300

    Fixed a page

--- ps5.py
+++ ps5.py
@@ -269,15 +269,15 @@ pages.append(Page(
 PAGE_TOPO,
 "https://www.topocentras.lt/zaidimu-kompiuteris-sony-playstation-5-digital.html",
 "//*[@id='productPage']/div/div/article/div[2]/div[2]/div/h1",
-"//*[@id='productPage']/div[3]/div[2]/div[2]/div/div[1]/div/div/div[3]/span",))
+"//*[@id='productPage']/div[2]/div/article/div[2]/div[2]/div[2]/div[1]/div[2]/div[1]/span",))
 
-# pages.append(Page(
-# "Digital",
-# PAGE_TOPO,
-# "https://www.topocentras.lt/zaidimu-pultas-sony-dualsense-ps5.html",
-# "//*[@id='productPage']/div[2]/div[2]/div[1]/h1",
-# "//*[@id='productPage']/div[3]/div[2]/div[2]/div/div[1]/div/div/div[3]/span",
-# test=True))
+pages.append(Page(
+"Digital",
+PAGE_TOPO,
+"https://www.topocentras.lt/zaidimu-pultas-sony-dualsense-ps5.html",
+"//*[@id='productPage']/div[2]/div[2]/div[1]/h1",
+"//*[@id='productPage']/div[2]/div/article/div[2]/div[2]/div[2]/div[1]/div[2]/div[1]/span",
+test=True))
 
 pages.append(Page(
 "Standard",
